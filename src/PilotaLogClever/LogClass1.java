2c69ea7e8cd05beae74b0a6b720f892a72838ca0
Riccardo Di Pietro <dipcisco@hotmail.com>
2014-02-07 18:45:21 +0100

    pulizia codice morto 07.02.2014

--- src/PilotaLogClever/LogClass1.java
+++ src/PilotaLogClever/LogClass1.java
@@ -80,20 +80,6 @@ public class LogClass1 {
    
    
    
-   
-   static void creaDir(String directoryName){
-        File theDir = new File(directoryName);
-
-  // if the directory does not exist, create it
-  if (!theDir.exists()) {
-    System.out.println("creating directory: " + directoryName);
-    boolean result = theDir.mkdir();  
-
-     if(result) {    
-       System.out.println("DIR created");  
-     }
-  }
-    }
-}//class
+  }//class
 
 
